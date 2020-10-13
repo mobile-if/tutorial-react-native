403861e978e49716ccf1080cfd7e0f8ce7eed8ea
syubban <syubbanfakhriya@gmail.com>
2020-10-13 23:35:02 +0700

    modul 4 done insyaallah, monggo tambahan-tambahannya

--- App.js
+++ App.js
@@ -21,12 +21,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
